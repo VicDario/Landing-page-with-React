ef5e7e1d4186ee75cb6224a55fefb2f12daccde6
RichiCorrea <richi.correa.f@gmail.com>
2021-06-07 18:37:30 +0000

    primer save

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import '@popperjs/core';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
