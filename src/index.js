272e934c3c0b675e027d6624294b6908040cc7e3
RichiCorrea <richi.correa.f@gmail.com>
2021-06-07 20:17:41 +0000

    nav con carperta components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import '@popperjs/core';
+import "bootstrap"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
