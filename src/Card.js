859e78be325a7649ca2425be687bc58e494db7e4
VicDario <26585356+VicDario@users.noreply.github.com>
2021-06-07 17:22:31 -0400

    Better cards

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const styles = {
-    width: '18rem'
+    width: '100%'
 }
 const Card = (props) => {
     return (
-        <div className="card" style={styles}>
-            <img src={props.imageSource} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+        <div className="card my-2" style={styles}>
+            <img src={props.imageSource} className="card-img-top" alt={props.imageAlt} />
+            <div className="card-body text-center">
+                <h5 className="card-title">{props.cardTitle}</h5>
+                <p className="card-text">{props.cardDescription}</p>
+                <a href={props.buttonHref} className="btn btn-primary">{props.buttonLabel}</a>
             </div>
         </div>
     )
